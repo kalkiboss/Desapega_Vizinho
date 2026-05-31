3fe70da9d7ae552bfd960395ab4d21231e0c12a9
manumanullll <manuela.dewes@iesb.edu.br>
2026-05-31 19:53:00 -0300

    feat(api): configura middlewares cors, limite de payload para json e escuta da porta 3000

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 const serviceAccount = JSON.parse(
     readFileSync(new URL('./serviceAccountKey.json', import.meta.url))
 );
@@ -12,5 +18,9 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-
 console.log('Firebase Admin SDK inicializado com sucesso e conectado ao Firestore!');
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(` API REST Privada do Desapega Vizinho rodando na porta ${PORT}`);
+});
